d80ea9601c4a4e61d31e4f0faac3c700f3b388ee
al0x99 <a.sfirschi@gmail.com>
2023-05-23 16:56:46 +0200

    new id

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 jQuery(function($) {
-    $('#fancy_icon-5727-581').on('click', function() {
+    $('#fancy_icon-13696-581').on('click', function() {
         $.post(MyScript.ajaxurl, {
             action: 'track_click',
             page_url: window.location.href
